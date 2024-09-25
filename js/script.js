37b69ccff00be428ff6d9c925946853ef9c93386
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-09-25 14:17:07 +0900

    feat :: header 스크롤 이벤트

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+// 스플리팅 호출
 document.addEventListener("DOMContentLoaded", function() {
     Splitting();
 });
+
+// 헤더 스크롤 방향 이벤트
+$(function() {
+    var prevScrollTop = 0;
+    document.addEventListener("scroll", function() {
+        var nowScrollTop = $(window).scrollTop();
+
+        if(nowScrollTop > prevScrollTop) {
+            $('header').addClass('active');
+        } else {
+            $('header').removeClass('active');
+        }
+        prevScrollTop = nowScrollTop;
+    })
+});
+

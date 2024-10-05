2ca3832650706303faacd530f4e29bbd00d172b7
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-10-05 17:26:49 +0900

    feat :: Gsap ScrollTrigger를 활용한 워크리스트제작

--- js/script.js
+++ js/script.js
@@ -53,4 +53,56 @@ $(function() {
         { 'clip-path': 'inset(0% 0% 0% 0% round 0%)', ease: 0, duration: 10 }, 
         0
       )
+
+    // .con02 gsap 애니메이션
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: '.con02',
+            start: '0% 100%',
+            end: '0% 20%',
+            scrub: 1,
+        }
+    })
+    .fromTo('.con02 .title .a',
+        {x: '-100%'},
+        {x: '0%', ease: 'none', duration: 5},
+        0
+    )
+    .fromTo('.con02 .title .b',
+        {x: '100%'},
+        {x: '0%', ease: 'none', duration: 5},
+        0
+    )
+
+    // workList show
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: '.workList',
+            start: '0% 100%',
+            end: '0% 100%',
+            scrub: 1
+        }
+    })
+    .to('.wrap',{
+        backgroundColor: "#000",
+        color: "#fff",
+        ease: 'none',
+        duration: 5
+    },0)
+
+    // title position
+    .to('.con02 .title', {
+        position: 'fixed',
+        ease: "none",
+        left: 0,
+        top: "25%",
+        width: "100%",
+        duration: 5
+    },0)
+
+    // workList animation 자연스럽게 수정하기
+    .fromTo('workList',
+        {margin: '0 auto'}, 
+        {margin: '100vh auto 0', position: 'relative', zIndex: '10', duration: 5},
+        0)
 });
